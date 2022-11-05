1e21147134789aa57ef96aa900174f1463c90b34
IvaschenkoAndrii <ivaschenko84@gmail.com>
2022-11-05 12:58:07 +0200

    1

--- src/app/components/exchange/exchange.component.ts
+++ src/app/components/exchange/exchange.component.ts
@@ -1,6 +1,5 @@
 import {Component, Input, OnInit} from '@angular/core';
-import {FormGroup} from "@angular/forms";
-
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 
 @Component({
@@ -19,14 +18,25 @@ export class ExchangeComponent implements OnInit {
   form: FormGroup;
 
 
-  constructor() { }
+  constructor() {
+    this._initForm()
+  }
 
   ngOnInit(): void {
   }
 
+  _initForm(): void {
+    this.form = new FormGroup({
+      select1: new FormControl(null,[Validators.required]),
+      select2: new FormControl(null,[Validators.required]),
+      input1: new FormControl(null, [Validators.required]),
+      input2: new FormControl(null, [Validators.required]),
+    })
+  }
 
   functionS1() {
     console.log('s1');
+
   }
 
   functionS2() {
@@ -35,9 +45,22 @@ export class ExchangeComponent implements OnInit {
 
   functionI1() {
     console.log('i1');
+    console.log(this.form.value.input1);
+    this.form.setValue({
+      input1: this.form.value.input1,
+      input2:this.form.value.input1,
+      select1:this.form.value.select1,
+      select2: this.form.value.select1
+    })
   }
 
   functionI2() {
     console.log('i2');
+    this.form.setValue({
+      input1: this.form.value.input2,
+      input2:this.form.value.input2,
+      select1:this.form.value.select1,
+      select2: this.form.value.select1
+    })
   }
 }
