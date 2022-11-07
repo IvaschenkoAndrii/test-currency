6f256edba54d7136b68baf322a51cca5700ef5c2
IvaschenkoAndrii <ivaschenko84@gmail.com>
2022-11-08 00:56:03 +0200

    Finish(adaptive)

--- src/app/components/exchange/exchange.component.ts
+++ src/app/components/exchange/exchange.component.ts
@@ -22,32 +22,50 @@ export class ExchangeComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.functionI1();
+    this.functionI2();
   }
 
   _initForm(): void {
     this.form = new FormGroup({
       select1: new FormControl(1, [Validators.required]),
       select2: new FormControl(1, [Validators.required]),
-      input1: new FormControl(null, [Validators.required]),
-      input2: new FormControl(null, [Validators.required]),
+      input1: new FormControl(null, [
+        Validators.required,
+        Validators.min(0)
+      ]),
+      input2: new FormControl(null, [
+        Validators.required,
+        Validators.min(0)
+      ]),
     })
   }
 
-  functionI1() {
-    this.form.setValue({
-      input1: this.form.value.input1,
-      input2:(this.form.value.input1*this.form.value.select1/this.form.value.select2).toFixed(2),
-      select1:this.form.value.select1,
-      select2: this.form.value.select2
-    })
+
+  functionI1(): void {
+    this.form.get('input1')?.valueChanges
+      .subscribe((v) =>
+        this.form.setValue({
+          input1: v,
+          input2: (v * 3 / 2).toFixed(2),
+          select1: 1,
+          select2: 1
+        })
+      )
   }
 
-  functionI2() {
-    this.form.setValue({
-      input1: (this.form.value.input2*this.form.value.select2/this.form.value.select1).toFixed(2),
-      input2: this.form.value.input2,
-      select1:this.form.value.select1,
-      select2: this.form.value.select2
-    })
+
+  functionI2(): void {
+    this.form.get('input2')?.valueChanges
+      .subscribe((c) =>
+        console.log(c)
+      // this.form.setValue({
+      //   input1: (c.input2 * c.select2 / c.select1).toFixed(2),
+      //   input2: c,
+      //   select1: c.select1,
+      //   select2: c.select2
+      // })
+    )
   }
+
 }
